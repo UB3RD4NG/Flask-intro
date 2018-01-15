3fc114d4c976af7058978fc1d59376a739b36fd3
Haoyu Deng <UB3RD4NG@Haoyus-MacBook-Air.local>
2018-01-15 12:18:14 -0600

    Chapter 2 initial commit - create HTML templates and point view to them

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,22 @@
+from flask import render_template
 from app import site
 
-@site.route("/")
-@site.route("/index")
+@site.route('/')
+@site.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': "Miguel"}
+    posts = [
+        {
+            'author': {'username': "John"},
+            'body': "Beautiful day in Portland!",
+            'date': "12/16/2017"
+        },
+        {
+            'author': {'username': "Susan"},
+            'body': "The Avengers movie was so cool!",
+            'date': "08/21/2015"
+        }
+    ]
+
+    #return render_template('index.html', user=user)
+    return render_template('index.html', title='Home', user=user, posts=posts)
