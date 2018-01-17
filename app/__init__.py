f52ae51e21a73cb5ffa78a4f2bf742ad81f2cc5e
Haoyu Deng <UB3RD4NG@Haoyus-MacBook-Air.local>
2018-01-17 17:15:20 -0600

    Chapter 3 initial commit - create and configure entry form

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from config import Config
 
 site = Flask(__name__)
+site.config.from_object(Config)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import site
+from app.forms import LoginForm
 
 @site.route('/')
 @site.route('/index')
@@ -17,6 +18,10 @@ def index():
             'date': "08/21/2015"
         }
     ]
-
-    #return render_template('index.html', user=user)
     return render_template('index.html', title='Home', user=user, posts=posts)
+    #return render_template('index.html', user=user)
+
+@site.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
